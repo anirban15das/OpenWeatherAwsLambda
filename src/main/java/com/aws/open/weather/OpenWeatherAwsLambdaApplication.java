1406db3f310866c465d39937adc0521e6551eff2
anirban15das <anirban16das@gmail.com>
2024-08-23 10:59:01 +0530

    Commit for AWS Case study

--- src/main/java/com/aws/open/weather/OpenWeatherAwsLambdaApplication.java
+++ src/main/java/com/aws/open/weather/OpenWeatherAwsLambdaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OpenWeatherAwsLambdaApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(OpenWeatherAwsLambdaApplication.class, args);
     }
